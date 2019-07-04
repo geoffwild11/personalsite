abefdae0b2c5658d6beabd1f7621dff9082aa511
Geoff Wild <geoff@zerohour-productions.net>
2019-07-04 14:27:27 -0500

    Link up some of the routing

--- geoffreywild-website/src/app/app-routing.module.ts
+++ geoffreywild-website/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+import { ResumeComponent } from './resume/resume.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/resume', pathMatch: 'full'},
+  { path: 'resume', component: ResumeComponent}
+];
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [ RouterModule.forRoot(routes)],
+  exports: [ RouterModule ]
 })
+
 export class AppRoutingModule { }
