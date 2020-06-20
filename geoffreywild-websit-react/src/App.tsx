07df5b7c7085c3422c717b14da865b75dc9444e9
geoffwild11 <geoffwild11@gmail.com>
2020-06-20 08:36:35 -0500

    Create a home page

--- geoffreywild-websit-react/src/App.tsx
+++ geoffreywild-websit-react/src/App.tsx
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id="header">
+        <h1>Geoffrey Wild</h1>
+        <nav>
+          <a href="#">Home</a>
+          <a href="#">Resume</a>
+        </nav>
+      </div>
+      <Home />
     </div>
   );
 }
