f2c266ea3fd6022b43353c3913cbba69a609fd55
Geoff Wild <geoffwild11@gmail.com>
2020-08-09 12:50:36 -0500

    Rearrange site (#13)
    * Center main content to cleanup look
    
    * Add visibility of system status resume buttons
    
    * adding some more cleanup
    
    * A little more cleanup
    
    * Moved about info to about page
    
    * Adjust home and add footer

--- geoffreywild-website-react/src/components/Resume.tsx
+++ geoffreywild-website-react/src/components/Resume.tsx
@@ -17,11 +17,19 @@ function Resume() {
     const [workExpanded, setWorkExpanded] = useState(true);
     const [educationExpanded, setEducationExpanded] = useState(true);
 
+    const workIconClassName: string = workExpanded ? "fas fa-caret-down" : "fas fa-caret-right";
+    const eduIconClassName: string = educationExpanded ? "fas fa-caret-down" : "fas fa-caret-right";
     return (<div>
-        <div className="expandButton" onClick={() => setWorkExpanded(!workExpanded)}>Work Experience</div>
-        {workExpanded ? <div className="collapsibleSection">{jobs.map((job: IJobInfo) => <Job jobInfo={job}/>)}</div> : <div />}
-        <div className="expandButton" onClick={() => setEducationExpanded(!educationExpanded)}>Education</div>
-        {educationExpanded ? <div className="collapsibleSection">{education.map((eduInfo: IEducationInfo) => <Education educationInfo={eduInfo}/>)}</div> : null}
+        <div className="expandButton" onClick={() => setWorkExpanded(!workExpanded)}>Work Experience<i className={`${workIconClassName} iconExpansion`} /></div>
+        {workExpanded 
+            ? <div className="collapsibleSection">{jobs.map((job: IJobInfo) => <Job jobInfo={job}/>)}</div> 
+            : <div />
+        }
+        <div className="expandButton" onClick={() => setEducationExpanded(!educationExpanded)}>Education<i className={`${eduIconClassName} iconExpansion`}/></div>
+        {educationExpanded 
+            ? <div className="collapsibleSection">{education.map((eduInfo: IEducationInfo) => <Education educationInfo={eduInfo}/>)}</div> 
+            : null
+        }
     </div>
     );
 }
