71e2110bba85632e5181b9bb993543cd60c1ad6d
geoffwild11 <geoffwild11@gmail.com>
2020-08-09 10:04:08 -0500

    Add visibility of system status resume buttons

--- geoffreywild-website-react/src/components/Resume.tsx
+++ geoffreywild-website-react/src/components/Resume.tsx
@@ -17,10 +17,12 @@ function Resume() {
     const [workExpanded, setWorkExpanded] = useState(true);
     const [educationExpanded, setEducationExpanded] = useState(true);
 
+    const workIconClassName: string = workExpanded ? "fas fa-caret-down" : "fas fa-caret-right";
+    const eduIconClassName: string = educationExpanded ? "fas fa-caret-down" : "fas fa-caret-right";
     return (<div>
-        <div className="expandButton" onClick={() => setWorkExpanded(!workExpanded)}>Work Experience</div>
+        <div className="expandButton" onClick={() => setWorkExpanded(!workExpanded)}>Work Experience<i className={`${workIconClassName} iconExpansion`} /></div>
         {workExpanded ? <div className="collapsibleSection">{jobs.map((job: IJobInfo) => <Job jobInfo={job}/>)}</div> : <div />}
-        <div className="expandButton" onClick={() => setEducationExpanded(!educationExpanded)}>Education</div>
+        <div className="expandButton" onClick={() => setEducationExpanded(!educationExpanded)}>Education<i className={`${eduIconClassName} iconExpansion`}/></div>
         {educationExpanded ? <div className="collapsibleSection">{education.map((eduInfo: IEducationInfo) => <Education educationInfo={eduInfo}/>)}</div> : null}
     </div>
     );
