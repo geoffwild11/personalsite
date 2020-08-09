e4ed2c93cfc0e65557154ecb625c6f0470fccb0a
geoffwild11 <geoffwild11@gmail.com>
2020-08-09 10:16:44 -0500

    adding some more cleanup

--- geoffreywild-website-react/src/components/Resume.tsx
+++ geoffreywild-website-react/src/components/Resume.tsx
@@ -21,9 +21,15 @@ function Resume() {
     const eduIconClassName: string = educationExpanded ? "fas fa-caret-down" : "fas fa-caret-right";
     return (<div>
         <div className="expandButton" onClick={() => setWorkExpanded(!workExpanded)}>Work Experience<i className={`${workIconClassName} iconExpansion`} /></div>
-        {workExpanded ? <div className="collapsibleSection">{jobs.map((job: IJobInfo) => <Job jobInfo={job}/>)}</div> : <div />}
+        {workExpanded 
+            ? <div className="collapsibleSection">{jobs.map((job: IJobInfo) => <Job jobInfo={job}/>)}</div> 
+            : <div />
+        }
         <div className="expandButton" onClick={() => setEducationExpanded(!educationExpanded)}>Education<i className={`${eduIconClassName} iconExpansion`}/></div>
-        {educationExpanded ? <div className="collapsibleSection">{education.map((eduInfo: IEducationInfo) => <Education educationInfo={eduInfo}/>)}</div> : null}
+        {educationExpanded 
+            ? <div className="collapsibleSection">{education.map((eduInfo: IEducationInfo) => <Education educationInfo={eduInfo}/>)}</div> 
+            : null
+        }
     </div>
     );
 }
